48ff3a3ea363113247caeaab75416648f79dbc0d
Aleh Kashnikau <aleh.kashnikau@gmail.com>
2016-04-02 18:51:25 +0300

    added fixes to project reader

--- src/Padawan/Framework/IO/Reader.php
+++ src/Padawan/Framework/IO/Reader.php
@@ -3,12 +3,21 @@
 namespace Padawan\Framework\IO;
 
 use Padawan\Domain\Core\Index;
+use \__PHP_Incomplete_Class;
 
 class Reader extends BasicIO {
     public function read($rootDir) {
-        return $this->prepare(
-            $this->readFromFile($this->getIndexFileName($rootDir))
-        );
+        try {
+            $project = $this->prepare(
+                $this->readFromFile($this->getIndexFileName($rootDir))
+            );
+            if ($project instanceof __PHP_Incomplete_Class) {
+                return;
+            }
+            return $project;
+        } catch (\Exception $e) {
+            return;
+        }
     }
     protected function prepare($rawProject) {
         $project = unserialize($rawProject);
